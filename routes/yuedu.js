4af23b017ecca4b6e4f14987420898733806ef61
MoonBegonia <moonbegonia@live.com>
2019-06-16 19:23:27 +0800

    add audio

--- routes/yuedu.js
+++ routes/yuedu.js
@@ -13,50 +13,50 @@ module.exports = async () => {
   let source = JSON.parse(fs.readFileSync(sourcePath));
   let sourceModTime = fs.statSync(sourcePath).mtime.toLocaleString();
   let invalid = []; //失效源
-  let discover = []; //发现
+  let genuine = []; //正版
   let r18 = []; //18禁
+  let audio = []; //有声
+  let discover = []; //发现
   let highQuality = []; //优
   let special = []; //xpath、json、正则
-  let genuine = []; //正版
-  let general = []; //普通
   let others = []; //待分类
   let full = []; //有效源
   let fullNOR18 = []; //有效源,没有18禁
   let fullIncludeInvalid = [];
   source.map(async (item) => {
-    const group = item.bookSourceGroup.toString();
+    const group = item.bookSourceGroup !== undefined ? item.bookSourceGroup.toString() : '';
     if (group.includes('失效')) {
       invalid.push(item);
     } else if (group.includes('正版')) {
       genuine.push(item);
-    } else if (group.includes('发现')) {
-      item.bookSourceGroup.toString().includes('优') ? item.bookSourceGroup : item.bookSourceGroup = '发现';
-      discover.push(item);
     } else if (group.includes('18禁')) {
       item.bookSourceGroup = '18禁';
       r18.push(item);
+    } else if (group.search(/有声/i) != -1) {
+      audio.push(item);
+    } else if (group.includes('发现')) {
+      group.search(/优|有声|JSON|JSon|XPath|正则/i) !== -1 ? item.bookSourceGroup : item.bookSourceGroup = '发现';
+      discover.push(item);
+    } else if (group.search(/JSON|JSon|XPath|正则/i) != -1) {
+      special.push(item);
     } else if (group.includes('优')) {
       item.bookSourceGroup = '优';
       highQuality.push(item);
-    } else if (group.search(/JSON|JSon|XPath|正则/i) != -1) {
-      special.push(item);
-    } else if (group.includes('普通')) {
-      general.push(item);
     } else {
-      item.bookSourceGroup = '待分类';
+      item.bookSourceGroup = '其他';
       others.push(item);
     }
   });
-  full = await full.concat(discover, r18, highQuality, special, general, genuine, others);
-  fullNOR18 = await fullNOR18.concat(discover, highQuality, special, general, genuine, others);
-  fullIncludeInvalid = await fullIncludeInvalid.concat(invalid, discover, r18, highQuality, general, genuine, others);
+  full = await full.concat(genuine, r18, discover, audio, special, highQuality, others);
+  fullNOR18 = await fullNOR18.concat(genuine, discover, audio, special, highQuality, others);
+  fullIncludeInvalid = await fullIncludeInvalid.concat(invalid, genuine, r18, discover, audio, special, highQuality, others);
   await write(path.join(__dirname, '../yuedu/invalid.json'), invalid);
   await write(path.join(__dirname, '../yuedu/genuine.json'), genuine);
-  await write(path.join(__dirname, '../yuedu/discover.json'), discover);
   await write(path.join(__dirname, '../yuedu/R18.json'), r18);
+  await write(path.join(__dirname, '../yuedu/audio.json'), audio);
+  await write(path.join(__dirname, '../yuedu/discover.json'), discover);
   await write(path.join(__dirname, '../yuedu/special.json'), special);
   await write(path.join(__dirname, '../yuedu/highQuality.json'), highQuality);
-  await write(path.join(__dirname, '../yuedu/general.json'), general);
   await write(path.join(__dirname, '../yuedu/others.json'), others);
   await write(path.join(__dirname, '../yuedu/fullNOR18.json'), fullNOR18);
   await write(path.join(__dirname, '../yuedu/full.json'), full);
@@ -71,11 +71,11 @@ module.exports = async () => {
 | - | - |
 |[失效](/yuedu/invalid.json)|${invalid.length}|
 |[正版](/yuedu/genuine.json)|${genuine.length}|
-|[发现](/yuedu/discover.json)|${discover.length}|
 |[18禁](/yuedu/R18.json)|${r18.length}|
+|[有声](/yuedu/audio.json)|${audio.length}|
+|[发现](/yuedu/discover.json)|${discover.length}|
 |[xpath、json、正则](/yuedu/special.json)|${special.length}|
 |[优](/yuedu/highQuality.json)|${highQuality.length}|
-|[普通](/yuedu/general.json)|${general.length}|
 |[待分类](/yuedu/others.json)|${others.length}|
 |[有效书源NOR18](/yuedu/fullNOR18.json)|${fullNOR18.length}|
 |[有效书源](/yuedu/full.json)|${full.length}|
