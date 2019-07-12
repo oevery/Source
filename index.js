1d1c952b45f53d292f402fec2d779fe726903323
MoonBegonia <moonbegonia@live.com>
2019-07-12 22:23:38 +0800

    add: index.js use readline achieve litle cli

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const yuedu = require('./routes/yuedu');
 const fangyuan = require('./routes/fangyuan');
+const getBookSource = require('./routes/getBookSource');
+const readline = require('readline');
 
-async function classify() {
-  await yuedu();
-  await fangyuan();
-}
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: ''
+});
+console.log('菜单：\n1: 书源分类;\n2: 视频源分类;\n3: 公众号书源获取;\n4: git repo 书源获取;\nclose: 退出');
 
-classify();
+rl.on('line', async (input) => {
+  switch (input.trim()) {
+    case '1':
+      await yuedu();
+      break;
+    case '2':
+      await fangyuan();
+      break;
+    case '3':
+      await getBookSource.getWeChatBookSource();
+      break;
+    case '4':
+      await getBookSource.getWeChatBookSource();
+      break;
+    case 'close':
+      rl.close();
+      break;
+    default:
+      console.log('请输入正确的命令！');
+      break;
+  }
+});
+
+rl.on('close', function () {
+  console.log('bye bye');
+  process.exit(0);
+});
